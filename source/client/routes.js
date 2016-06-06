c3545613796b8b1f030d96f25ce10833b73deca1
Seongjun Kim <isair.zun@gmail.com>
2016-06-07 02:14:18 +0900

    Home Page

--- source/client/routes.js
+++ source/client/routes.js
@@ -5,9 +5,9 @@ import { syncHistoryWithStore } from 'react-router-redux'
 import store from './store'
 
 import App from './containers/App'
-import Home from './containers/Home'
+// import Home from './containers/Home'
 import MangaHome from './containers/MangaHome'
-import MangaInfo from './containers/MangaInfo'
+// import MangaInfo from './containers/MangaInfo'
 import MangaViewer from './containers/MangaViewer'
 import NotFound from './containers/NotFound'
 
@@ -24,7 +24,7 @@ const history = syncHistoryWithStore(browserHistory, store)
 export default (
   <Router history={history}>
     <Route path="/" component={App}>
-      <IndexRoute component={Home} />
+      <IndexRoute component={MangaHome} />
       <Route path="/manga" component={MangaHome} />
       <Route path="/manga/:id" component={MangaViewer} />
 
